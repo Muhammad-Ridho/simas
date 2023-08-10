e9f2f5362ff59da0f973d0531834f3f7d3c42026
Muhammad-Ridho <ramadhanjenius1@gmail.com>
2023-08-10 15:03:05 +0700

    menyesuaikan tampilan home, navbar & sidebar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
                 @csrf
                 <div class="input-group mb-3">
                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-envelope"></span>
@@ -35,7 +35,7 @@
                 </div>
                 <div class="input-group mb-3">
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                        name="password" required autocomplete="current-password">
+                        name="password" required autocomplete="current-password" placeholder="Password">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,22 +5,52 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">{{ Auth::user()->name }}</a>
+            <a href="#" class="nav-link active">SYSTEM PP. YATIM DHUAFA AL-KHLAS SINGOSARI</a>
         </li>
     </ul>
 
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">        
+    <ul class="navbar-nav ml-auto mr-3">
         <li class="nav-item">
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                <button type="button" class="btn btn-danger">{{ __('LogOuut') }}</button>
+
+
+        </li>
+
+        <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                <span class="hidden-xs">{{ Auth::user()->name }}</span>
             </a>
+            <ul class="dropdown-menu">
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
+                <li class="user-header">
+                    <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                    <p>
+                        {{ Auth::user()->name }}
+                        <small>Role User</small>
+                    </p>
+                </li>
 
+                <li class="user-footer p-3 text-center">
+                    <div class="row ml-5">
+                        <div class="pull-left">
+                            <a href="#" class="nav-item">
+                                <button type="button" class="btn btn-primary">Profile</button>
+                            </a>
+                        </div>
+                        <div class="pull-right pl-3">
+                            <a class="nav-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                                <button type="button" class="btn btn-danger">{{ __('Logout') }}</button>
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </li>
     </ul>
 </nav>
